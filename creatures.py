7fb84088a49c0b5f3f62ee8cd41cf069e197d380
Ishai Masada <imasada@vtcap.org>
2021-07-02 10:55:42 -0400

    init

--- creatures.py
+++ creatures.py
@@ -1,89 +1,26 @@
-class mantois:
-    def __init__(self, level=0, name="mantois", hp=16, strength=4, defense=1, accuracy=10, resistance=0, team_skill_strength=0, power=2):
+import json
+
+class Creature:
+    def __init__(self, level, name, type, hp, strength, defense, accuracy, resistance, speed):
         self.level = level
         self.name = name
-        self.power = power
-        self.hp = hp + (level * power)
-        self.strength = strength + (level * power)
-        self.defense = defense + (level * power)
-        self.accuracy = accuracy + (level * power)
-        self.resistance = resistance + (level * power)
-        self.team_skill_strength = team_skill_strength + (level * power)
+        self.type = type
+        self.hp = hp
+        self.strength = strength
+        self.speed = speed
+        self.defense = defense
+        self.accuracy = accuracy
+        self.resistance = resistance
 
+    @classmethod
+    def from_json(cls, creature_json):
+        return cls(level=creature_json['level'], type=creature_json['type'], name=creature_json['name'], hp=creature_json['hp'], strength=creature_json['strength'], defense=creature_json['defense'], accuracy=creature_json['accuracy'], resistance=creature_json['resistance'], speed=creature_json['speed'])
 
-class bitbit:
-    def __init__(self, level=0, name="bitbit", hp=20, strength=7, defense=3, accuracy=11, resistance=0, team_skill_strength=0, power=3):
-        self.level = level
-        self.name = name
-        self.power = power
-        self.hp = hp + (level * power)
-        self.strength = strength + (level * power)
-        self.defense = defense + (level * power)
-        self.accuracy = accuracy + (level * power)
-        self.resistance = resistance + (level * power)
-        self.team_skill_strength = team_skill_strength + (level * power)
+if __name__ == '__main__':
+    with open('creatures.json', 'r') as f:
+        creatures_json = json.load(f)
 
+    creatures = {}
+    for name, stats in creatures_json.items():
+        creatures[name] = Creature.from_json(stats)
 
-class woorm:
-    def __init__(self, level=0, name="woorm", hp=20, strength=6, defense=6, accuracy=8, resistance=1, team_skill_strength=1, power=3):
-        self.level = level
-        self.name = name
-        self.power = power
-        self.hp = hp + (level * power)
-        self.strength = strength + (level * power)
-        self.defense = defense + (level * power)
-        self.accuracy = accuracy + (level * power)
-        self.resistance = resistance + (level * power)
-        self.team_skill_strength = team_skill_strength + (level * power)
-
-
-class rocklit:
-    def __init__(self, level=0, name="rocklit", hp=12, strength=3, defense=13, accuracy=4, resistance=4, team_skill_strength=0, power=2):
-        self.level = level
-        self.name = name
-        self.power = power
-        self.hp = hp + (level * power)
-        self.strength = strength + (level * power)
-        self.defense = defense + (level * power)
-        self.accuracy = accuracy + (level * power)
-        self.resistance = resistance + (level * power)
-        self.team_skill_strength = team_skill_strength + (level * power)
-
-
-class moonkier:
-    def __init__(self, level=0, name="moonkier", hp=16, strength=8, defense=5, accuracy=6, resistance=7, team_skill_strength=0, power=4):
-        self.level = level
-        self.name = name
-        self.power = power
-        self.hp = hp + (level * power)
-        self.strength = strength + (level * power)
-        self.defense = defense + (level * power)
-        self.accuracy = accuracy + (level * power)
-        self.resistance = resistance + (level * power)
-        self.team_skill_strength = team_skill_strength + (level * power)
-
-
-class pythooner:
-    def __init__(self, level=0, name="pythooner", hp=24, strength=12, defense=7, accuracy=9, resistance=8, team_skill_strength=3, power=5):
-        self.level = level
-        self.name = name
-        self.power = power
-        self.hp = hp + (level * power)
-        self.strength = strength + (level * power)
-        self.defense = defense + (level * power)
-        self.accuracy = accuracy + (level * power)
-        self.resistance = resistance + (level * power)
-        self.team_skill_strength = team_skill_strength + (level * power)
-
-
-class dragoooner:
-    def __init__(self, level=0, name="dragoooner", hp=25, strength=15, defense=9, accuracy=7, resistance=10, team_skill_strength=1, power=6):
-        self.level = level
-        self.name = name
-        self.power = power
-        self.hp = hp + (level * power)
-        self.strength = strength + (level * power)
-        self.defense = defense + (level * power)
-        self.accuracy = accuracy + (level * power)
-        self.resistance = resistance + (level * power)
-        self.team_skill_strength = team_skill_strength + (level * power)
