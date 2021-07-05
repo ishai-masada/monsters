512a3ae2dd4d813e9149aeece0db7dda34702414
Ishai Masada <imasada@vtcap.org>
2021-07-05 13:34:21 -0400

    Changed the creature(s)_json to creature_json

--- creatures.py
+++ creatures.py
@@ -1,6 +1,10 @@
 import json
 
 class Creature:
+
+    def __repr__(self):
+        return f"<Name: {self.name}, Health: {self.hp}>"
+
     def __init__(self, level, name, _type, hp, strength, defense, accuracy, resistance, speed):
         self.level = level
         self.name = name
@@ -14,13 +18,13 @@ class Creature:
 
     @classmethod
     def from_json(cls, creature_json):
-        return cls(level=creature_json['level'], type=creature_json['type'], name=creature_json['name'], hp=creature_json['hp'], strength=creature_json['strength'], defense=creature_json['defense'], accuracy=creature_json['accuracy'], resistance=creature_json['resistance'], speed=creature_json['speed'])
+        return cls(level=creature_json['level'], _type=creature_json['type'], name=creature_json['name'], hp=creature_json['hp'], strength=creature_json['strength'], defense=creature_json['defense'], accuracy=creature_json['accuracy'], resistance=creature_json['resistance'], speed=creature_json['speed'])
 
 if __name__ == '__main__':
     with open('creatures.json', 'r') as f:
-        creatures_json = json.load(f)
+        creature_json = json.load(f)
 
     creatures = {}
-    for name, stats in creatures_json.items():
+    for name, stats in creature_json.items():
         creatures[name] = Creature.from_json(stats)
 
