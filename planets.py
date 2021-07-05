0420f61d6b10bb7f4781a6531c7a774fc20a1132
Ishai Masada <imasada@vtcap.org>
2021-07-05 13:35:09 -0400

    Copied creatures.py format for planets

---
+++ planets.py
@@ -0,0 +1,24 @@
+
+import json
+
+class Planet:
+
+    def __repr__(self):
+        return f"<Name: {self.name}>"
+
+    def __init__(self, number_of_levels, name, levels):
+        self.level = number_of_levels,
+        self.name = name
+        self.levels = levels
+
+    @classmethod
+    def from_json(cls, planet_json):
+        return cls(name=planet_json["name"], number_of_levels=planet_json["number_of_levels"], levels=planet_json["levels"])
+
+if __name__ == '__main__':
+    with open('planets.json', 'r') as f:
+        planets_json = json.load(f)
+
+    planets = {}
+    for name, stats in planets_json.items():
+        planets[stats] = Planet.from_json(stats)
