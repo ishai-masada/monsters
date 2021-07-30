7760589f62e7d60d9465e3b3e2e9ca6e9cdf755c
Ishai Masada <imasada@vtcap.org>
2021-07-30 16:02:14 -0400

    added deepcopy
    copied player and enemy creatures.

--- conquest.py
+++ conquest.py
@@ -1,4 +1,5 @@
 import random
+from copy import deepcopy
 import json
 from planets import Planet
 from creatures import Creature
@@ -30,7 +31,26 @@ def load_planets():
         planets.append(Planet.from_json(planet))
     return planets
 
+def battle_attack(enemy):
+    if rand_bool():
+        enemy.hp -= 10
+        print(f"\nYour attack hit the {enemy.name}!") 
+    else:
+        print("\nYour attack missed! You stoopid.")
+    enemy_display(enemy)
+
+def battle_run(enemy):
+    if rand_bool():
+        print("\nYou ran away from your duty you cooward!")
+        return 'break'
+    else:
+        print("\nYou failed to run away you cooward!")
+        return 'continue'
+
+BATTLE_MENU = {'attack': battle_attack, 'run': battle_run}
+
 def battle(enemy):
+    print(f'\nThis is the enemy argument: {enemy}')
     print(f"\nYou are fighting a {enemy.name}!")
     count = 0
     while True:
@@ -40,10 +60,10 @@ def battle(enemy):
             print(f"{enemy.name} attacks you!")
             if rand_bool():
                 player.hp -= 4
-                print("{enemy.name}'s attack hit you!\n")
+                print(f"{enemy.name}'s attack hit you!\n")
                 display()
             else:
-                print("{enemy.name}'s attack missed you!\n")
+                print(f"{enemy.name}'s attack missed you!\n")
                 display()
         # Player turn
         else:
@@ -52,27 +72,15 @@ def battle(enemy):
                 choice = input("\nIt's time to act!. Either attack or run away like a cooward. Type either \'attack\', or \'run\'. "
                                "\nYou will have to retype your input if you misspell it: ")
                 # Check if the input is spelled correctly
-                if choice in ['attack', 'run']:
+                if choice in BATTLE_MENU:
                     break
                 # Create an error if it isn't spelled correctly
                 print("\nYour input did not read as \"attack\" or \"run\".")
-
-            # Check if it's attack
-            if choice == "attack":
-                if rand_bool():
-                    enemy.hp -= 10
-                    print(f"\nYour attack hit the {enemy.name}!") 
-                else:
-                    print("\nYour attack missed! You stoopid.")
-                enemy_display(enemy)
-            # Check if it's run
-            elif choice == "run":
-                if rand_bool():
-                    print("\nYou ran away from your duty you cooward!")
-                    break
-                else:
-                    print("\nYou failed to run away you cooward!")
-                    continue
+            prompt = BATTLE_MENU[choice](enemy)
+            if prompt == 'break':
+                break
+            elif prompt == 'continue':
+                continue
             # Checks for victory or loss each round of turns
         if player.hp<=0 and enemy.hp>0:
             print("\nYou have lost! Return to the home you came from.")
@@ -81,15 +89,14 @@ def battle(enemy):
             print("\nYou have defeated your enemy and attained victory! Congratulations!")
             break
     return 0
-
 # Start of the game
 print("Welcome to Conquest!", "\nYour goal is to conquer every planet and establish peace.")
-input("press enter to start the first battle...")
+# input("press enter to start the first battle...")
 creatures = load_creatures()
 planets = load_planets()
 
 inventory = []
-player = creatures.pop(random.choice(list(creatures)))
+player = deepcopy(creatures.get(random.choice(list(creatures))))
 
 # print(f"The first battle is with a {enemy} on planet {planets} \n")
 # print(f"{enemy.name} has a health of {enemy.hp} and a strength of {enemy.strength}. Diminish its health to defeat it!")
@@ -98,6 +105,7 @@ display()
 
 level_1 = planets[0].levels[0]
 print(f"This is the first level : {level_1}")
-for creature in level_1.get('creatures'):
-    creature = creatures.get(creature)
-    battle(creature)
+monsters = level_1.get('creatures')
+for creature in monsters:
+    monster = deepcopy(creatures.get(creature))
+    battle(monster)
