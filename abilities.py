f6bd99774673f993f02ec70cdef2937cfb4fceaf
Ishai Masada <imasada@vtcap.org>
2021-08-09 11:52:48 -0400

    Added player argument

--- abilities.py
+++ abilities.py
@@ -1,29 +1,29 @@
 ### Abilities
 
-def hard_punch(enemy):
+def hard_punch(player, enemy):
 	enemy.hp -= 15 
 	return f'enemy hp is now: {enemy.hp}'
 
-def wiggle(enemy):
+def wiggle(player, enemy):
 	enemy.hp -= 17
 	return f'enemy hp is now: {enemy.hp}'
 
-def fire_attack(enemy):
+def fire_attack(player, enemy):
 	enemy.hp -= 20
 	return f'enemy hp is now: {enemy.hp}'
 
-def nibble(enemy):
+def nibble(player, enemy):
 	enemy.hp -= 9
 	return f'enemy hp is now: {enemy.hp}'
 
-def crush(enemy):
+def crush(player, enemy):
 	enemy.hp -= 3
 	return f'enemy hp is now: {enemy.hp}'
 
-def spear_attack(enemy):
+def spear_attack(player, enemy):
 	enemy.hp -= 3
 	return f'enemy hp is now: {enemy.hp}'
 
-def sting(enemy):
+def sting(player, enemy):
 	enemy.hp -= 15
 	return f'enemy hp is now: {enemy.hp}'
