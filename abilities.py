f92e78f2b9972ca47127a36dc7f624ca2a73996e
Ishai Masada <imasada@vtcap.org>
2021-08-22 14:14:45 -0400

    Changed the amount of damage

--- abilities.py
+++ abilities.py
@@ -2,28 +2,32 @@
 
 def hard_punch(player, enemy):
 	enemy.hp -= 15 
-	return f'enemy hp is now: {enemy.hp}'
+	return f'hp has been reduced to: {enemy.hp}'
+
+def push(player, enemy):
+    enemy.hp -= 13
+    return f'hp has been reduced to: {enemy.hp}'
 
 def wiggle(player, enemy):
 	enemy.hp -= 17
-	return f'enemy hp is now: {enemy.hp}'
+	return f'hp  has been reduced to: {enemy.hp}'
 
 def fire_attack(player, enemy):
 	enemy.hp -= 20
-	return f'enemy hp is now: {enemy.hp}'
+	return f'hp  has been reduced to: {enemy.hp}'
 
 def nibble(player, enemy):
 	enemy.hp -= 9
-	return f'enemy hp is now: {enemy.hp}'
+	return f'hp  has been reduced to: {enemy.hp}'
 
 def crush(player, enemy):
 	enemy.hp -= 3
-	return f'enemy hp is now: {enemy.hp}'
+	return f'hp  has been reduced to: {enemy.hp}'
 
 def spear_attack(player, enemy):
 	enemy.hp -= 3
-	return f'enemy hp is now: {enemy.hp}'
+	return f'hp  has been reduced to: {enemy.hp}'
 
 def sting(player, enemy):
 	enemy.hp -= 15
-	return f'enemy hp is now: {enemy.hp}'
+	return f'hp  has been reduced to: {enemy.hp}'
