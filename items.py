c74790d0dadc0109ab96a346347542a93313d078
Ishai Masada <imasada@vtcap.org>
2021-09-01 11:59:43 -0400

    Changed the arguement
    Adjusted for the entire save state as an argument instead of the player property

--- items.py
+++ items.py
@@ -1,5 +1,5 @@
 ### Items
 
-def small_recover(player, enemy):
-    player.hp += 10
-    return f'player\'s hp is now {player.hp}'
+def small_recover(player_monster, enemy):
+    player_monster.player.hp += 10
+    return f'player\'s hp is now {player_monster.player.hp}'
